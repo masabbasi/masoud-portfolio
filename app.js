d536ac4565e937e4a747a2bd13ac66ba7b58ce43
MasAbbasi <masoud.abbasi.71@gmail.com>
2023-01-28 12:59:43 +0330

    All

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 window.addEventListener('scroll', () => {
-    if(window.scrollY < 500){
+    if(window.scrollY < 1200){
         document.querySelector('.html').setAttribute('style', "width :0%")
         document.querySelector('.css').setAttribute('style', "width :0%")
         document.querySelector('.js').setAttribute('style', "width :0%")
         document.querySelector('.photoshop').setAttribute('style', "width :0%")
         document.querySelector('.illustrator').setAttribute('style', "width :0%")
     }
-    else if(window.scrollY > 500){
+    else if(window.scrollY > 1200){
         document.querySelector('.html').setAttribute('style', "width :90%")
         document.querySelector('.css').setAttribute('style', "width :80%")
         document.querySelector('.js').setAttribute('style', "width :10%")
